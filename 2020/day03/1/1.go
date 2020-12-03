7bacd6b5e20501a6048fac2a3a475de7fdb0e8a5
Laurence de Jong <info@ldej.nl>
2020-12-03 11:00:39 +0530

    2020 day03

---
+++ 2020/day03/1/1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ldej/advent-of-code/tools"
+)
+
+func main() {
+	result := run()
+	fmt.Println(result)
+}
+
+func run() int {
+	grid := tools.ReadRuneGrid("./2020/day03/input.txt")
+
+	row := 0
+	column := 0
+	trees := 0
+
+	for {
+		column = (column + 3) % len(grid[0])
+		row += 1
+
+		if row >= len(grid) {
+			break
+		}
+		if grid.At(row, column) == '#' {
+			trees += 1
+		}
+	}
+	return trees
+}
---
+++ 2020/day03/2/2.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ldej/advent-of-code/tools"
+)
+
+func main() {
+	result := run()
+	fmt.Println(result)
+}
+
+func run() int {
+	grid := tools.ReadRuneGrid("./2020/day03/input.txt")
+
+	total := trees(grid, 1,1)
+	total *= trees(grid, 1,3)
+	total *= trees(grid, 1,5)
+	total *= trees(grid, 1,7)
+	total *= trees(grid, 2,1)
+
+	return total
+}
+
+func trees(grid tools.RuneGrid, rowSlope int, columnSlope int) int {
+	row := 0
+	column := 0
+	trees := 0
+
+	for {
+		column = (column + columnSlope) % len(grid[0])
+		row += rowSlope
+
+		if row >= len(grid) {
+			break
+		}
+		if grid.At(row, column) == '#' {
+			trees += 1
+		}
+	}
+	return trees
+}
